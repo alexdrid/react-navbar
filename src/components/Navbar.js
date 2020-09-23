5b4337274286c4dced33d3c832f165b4cec1f076
alexdrid <alexdrid13@gmail.com>
2020-09-23 23:16:18 +0300

    2 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,17 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 
+const navMenuItems = ["Home", "About", "Services"];
 export const Navbar = () => {
-  return <nav className="navbar">Navbar</nav>;
+  return (
+    <nav className="navbar">
+      Navbar
+      <input type='text' placeholder='Search'></input>
+      <div className='menu-container'>
+        {navMenuItems.map(item => 
+          <li className="menu-item">{item}</li>
+        )}
+      </div>
+    </nav>
+  );
 };

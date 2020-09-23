e1f1c74936543c31ac816de0ef47883a1533ebce
alexdrid <alexdrid13@gmail.com>
2020-09-23 23:16:54 +0300

    1 file modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ export const Navbar = () => {
   return (
     <nav className="navbar">
       Navbar
-      <input type='text' placeholder='Search'></input>
       <div className='menu-container'>
         {navMenuItems.map(item => 
           <li className="menu-item">{item}</li>

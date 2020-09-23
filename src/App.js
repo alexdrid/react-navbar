eb52f96bd2dc48735dec2244d7f54df8200002ae
alexdrid <alexdrid13@gmail.com>
2020-09-23 21:10:50 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from './components/Navbar'
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>React Navbar</h1>
+      <Navbar/>
     </div>
   );
 }

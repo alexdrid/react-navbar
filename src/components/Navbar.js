8893161fc98c92f88b4001b3815f877071aaa7a9
dRed <alexdrid13@gmail.com>
2020-09-25 21:57:21 +0300

    Fixing styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaCode, FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { MdNavigation } from "react-icons/md";
 import "./Navbar.css";
 
 export const Navbar = () => {
+  const [isClicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
+  const closeMenu = () => {
+    setClicked(false);
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
+      <Link to="/home" className="navbar-logo">
         <MdNavigation className="logo-icon" />
         Navbar
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        {isClicked ? <FaTimes /> : <FaBars />}
       </div>
-
-      <div className="menu-container">
-        <div className="menu-icon">
-          <FaBars />
-        </div>
-        <Link to="/home" className="menu-item">
+      <div className={isClicked ? "menu-container active" : "menu-container"}>
+        <Link to="/home" className="menu-item" onClick={closeMenu}>
           Home
         </Link>
-        <Link to="/about" className="menu-item">
+        <Link to="/about" className="menu-item" onClick={closeMenu}>
           About
         </Link>
-        <Link to="/services" className="menu-item">
+        <Link to="/services" className="menu-item" onClick={closeMenu}>
           Services
         </Link>
       </div>

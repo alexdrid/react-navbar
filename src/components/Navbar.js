7902c1ca919aea8ea8f27497b65c71c264c81b66
dRed <alexdrid13@gmail.com>
2020-09-25 18:54:05 +0300

    Adding font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Navbar.css";
 
 const navMenuItems = ["Home", "About", "Services"];
+
 export const Navbar = () => {
   return (
     <nav className="navbar">
       Navbar
-      <div className='menu-container'>
-        {navMenuItems.map(item => 
+      <div className="menu-container">
+        {navMenuItems.map((item) => (
           <li className="menu-item">{item}</li>
-        )}
+        ))}
       </div>
     </nav>
   );
